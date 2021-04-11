1c40ab67f19897a9573095393780018dabdc2436
Shimizu Daisuke <>
2021-04-12 03:45:45 +0900

    Initial commit（エージェント作成のテンプレート）

---
+++ src/jp/ac/tsukuba/s/s2020602/SashimiRoleAssignPlayer.java
@@ -0,0 +1,11 @@
+package jp.ac.tsukuba.s.s2020602;
+
+import org.aiwolf.sample.lib.AbstractRoleAssignPlayer;
+
+public class SashimiRoleAssignPlayer extends AbstractRoleAssignPlayer {
+    
+    @Override
+    public String getName() {
+        return null;
+    }
+}
